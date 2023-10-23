7a2f1af7ad32904f34e28df0cb0c75c32722b411
MacJ13 <jacdim02@gmail.com>
2023-10-23 15:27:30 +0200

    Create gameplayView class

---
+++ src/js/modules/view/gameplayView.js
@@ -0,0 +1,129 @@
+class GameplayView {
+  gameEl = document.querySelector(".game");
+  gamePlayer1El = document.getElementById("user");
+  gamePlayer2El = document.getElementById("computer");
+
+  gameControlsEl = this.gamePlayer1El.querySelector(".game-controls");
+  gameShipPickEl = this.gamePlayer1El.querySelector(".game-ship-pick");
+
+  gameShipObjectEl = this.gamePlayer1El.querySelector(".game-ship-object");
+  gameShipAmountEl = this.gamePlayer1El.querySelector(".game-ship-amount");
+
+  draggedEl = null;
+
+  showGameplay() {
+    this.gameEl.classList.remove("hidden");
+    this.gameControlsEl.classList.remove("hidden");
+    this.showShipPick();
+  }
+
+  renderGameplay(players) {
+    players.forEach((player) => {
+      const type = player.getType();
+      const name = player.getName();
+      const board = player.getPlayerBoard();
+      const ships = player.getShips();
+
+      const playerEl = document.getElementById(type);
+      const playerNameEl = playerEl.querySelector(".game-playername");
+      const gameboardEl = playerEl.querySelector(".game-board");
+      const shipListEl = playerEl.querySelector(".game-list-ship");
+
+      this.renderPlayername(playerNameEl, name);
+      this.renderGameboard(gameboardEl, board);
+      this.renderShipList(shipListEl, ships);
+    });
+
+    this.showGameplay();
+  }
+
+  renderPlayername(element, name) {
+    const playername = name || "unknown";
+    element.textContent = playername + "'s board";
+  }
+
+  renderGameboard(element, board) {
+    element.innerHTML = "";
+
+    const size = board.length * board.length;
+
+    for (let i = 0; i < size; i++) {
+      let position = ("0" + i).slice(-2);
+      const [posA, posB] = position.split("");
+
+      const span = document.createElement("span");
+
+      const cls = board[+posA][+posB].shipCell
+        ? "game-cell game-cell-ship"
+        : "game-cell";
+
+      span.className = cls;
+
+      span.dataset.posA = posA;
+      span.dataset.posB = posB;
+
+      element.appendChild(span);
+    }
+  }
+
+  renderShipList(el, ships) {
+    el.innerHTML = "";
+
+    for (let i = 0; i < ships.length; i++) {
+      const li = document.createElement("li");
+      const isSunk = ships[i].getSunk();
+      const shipLength = ships[i].getLength();
+
+      const cls = isSunk ? "game-item-ship sunk" : "game-item-ship";
+
+      li.classList.add(cls);
+
+      for (let j = 0; j < shipLength; j++) {
+        const span = document.createElement("span");
+        span.className = "game-item-part";
+        li.appendChild(span);
+      }
+      el.appendChild(li);
+    }
+  }
+
+  renderShipPick(ship, count) {
+    this.showShipPick();
+    this.gameShipObjectEl.innerHTML = "";
+
+    if (!ship) {
+      this.hideShipPick();
+
+      return;
+    }
+
+    const shipLength = ship.getLength();
+
+    for (let i = 0; i < shipLength; i++) {
+      const span = document.createElement("span");
+      span.className = "game-ship-part";
+
+      this.gameShipObjectEl.appendChild(span);
+    }
+
+    const { direction } = this.gameShipObjectEl.dataset;
+
+    if (direction === "horizontal") {
+      this.gameShipObjectEl.style.gridTemplateColumns = `repeat(${this.gameShipObjectEl.children.length}, 1fr)`;
+    } else {
+      this.gameShipObjectEl.style.gridTemplateColumns = `repeat(1, 1fr)`;
+    }
+
+    this.gameShipAmountEl.textContent = `x${count}`;
+  }
+
+  showShipPick() {
+    this.gameShipPickEl.classList.remove("hidden");
+  }
+
+  hideShipPick() {
+    this.gameShipPickEl.classList.add("hidden");
+  }
+}
+
+export default GameplayView;

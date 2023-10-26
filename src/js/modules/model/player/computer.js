a4fca73ea2a0951a05da3ccaf3c8ff6a21566906
MacJ13 <jacdim02@gmail.com>
2023-10-26 15:16:14 +0200

    add properties to Computer Class
    add availablePositions, shipHit, ship,
    position

--- src/js/modules/model/player/computer.js
+++ src/js/modules/model/player/computer.js
@@ -1,6 +1,11 @@
 import Player from "./player";
 
 class Computer extends Player {
+  availablePositions = [];
+  shipHit = false;
+  ship;
+  position;
+
   constructor(type = "computer") {
     super(type);
   }

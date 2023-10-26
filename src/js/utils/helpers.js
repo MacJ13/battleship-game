920e658d70b3291ad02ef261735a9a09b8645d0b
MacJ13 <jacdim02@gmail.com>
2023-10-26 13:07:58 +0200

    Create 'binarySearch' function

--- src/js/utils/helpers.js
+++ src/js/utils/helpers.js
@@ -5,3 +5,29 @@ const concatNumbers = (pos) => {
 
   return Number(str);
 };
+
+export const binarySearch = function (arr, index) {
+  let left = 0;
+  let right = arr.length - 1;
+  let mid;
+
+  const indexNumber = concatNumbers(index);
+
+  while (right >= left) {
+    mid = left + Math.floor((right - left) / 2);
+
+    // if the element is present at the middle itsef
+
+    const midNumber = concatNumbers(arr[mid]);
+    if (midNumber === indexNumber) return mid;
+
+    // if element is smalled then mid, then
+    // it can only be present in the left subaaray
+    if (midNumber > indexNumber) right = mid - 1;
+    // otherwise the element can only be present
+    // in the right subarray
+    else left = mid + 1;
+  }
+
+  return -1;
+};

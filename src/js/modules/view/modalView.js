d4419b36a59f6d83987a9d7696bd0084fb45be10
MacJ13 <jacdim02@gmail.com>
2023-10-27 00:01:51 +0200

    Create modalView class

---
+++ src/js/modules/view/modalView.js
@@ -0,0 +1,24 @@
+class ModalView {
+  modalEl = document.querySelector(".modal");
+  resultEl = document.querySelector(".result");
+  restartBtnEl = document.getElementById("restart");
+
+  toggleModal() {
+    this.modalEl.classList.toggle("hidden");
+  }
+
+  renderGameResult(name) {
+    console.log(name);
+    this.resultEl.textContent = `${name} has won!`;
+  }
+
+  onClickRestartBtn(cb) {
+    this.restartBtnEl.addEventListener("click", cb);
+  }
+
+  removeClickRestartBtn(cb) {
+    this.restartBtnEl.removeEventListener("click", cb);
+  }
+}
+
+export default ModalView;

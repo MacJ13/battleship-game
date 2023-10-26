5a91bce5976e03121687b4bfbd46d77a50ce648f
MacJ13 <jacdim02@gmail.com>
2023-10-26 22:15:47 +0200

    Test 'clearReservedPositions' method

--- src/js/modules/model/player/__tests__/computer.test.js
+++ src/js/modules/model/player/__tests__/computer.test.js
@@ -1,8 +1,10 @@
+import Ship from "../../ship/ship";
 import Computer from "../computer";
 import Player from "../player";
 
 describe("Computer class", () => {
   const computer = new Computer();
+  computer.addGameboardPositions();
   test("tests object", () => {
     expect(computer).toBeTruthy();
     expect(computer instanceof Computer).toBeTruthy();
@@ -11,5 +13,26 @@ describe("Computer class", () => {
     expect(computer.getName()).toBeDefined();
     expect(computer.getName()).toBe("computer");
     expect(computer.getPlayerBoard()).toBeDefined();
+  });
+
+  test("tests computer properties", () => {
+    expect(computer.availablePositions.length).toBe(100);
+    expect(computer.shipHit).toBeFalsy();
+  });
+
+  test("tests clearReservedPositions() method in the middle board", () => {
+    const ship = new Ship(4);
+    computer.addShipOnPlayerGameboard(4, 4, ship);
+    computer.clearReservedPositions(ship.getReservedPositions());
+
+    expect(computer.availablePositions.length).toBe(86);
+  });
+
+  test("tests clearReservedPositions() method on the verge board", () => {
+    const ship = new Ship(3);
+    computer.addShipOnPlayerGameboard(0, 7, ship);
+    computer.clearReservedPositions(ship.getReservedPositions());
+
+    expect(computer.availablePositions.length).toBe(81);
   });
 });

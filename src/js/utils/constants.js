40bc7daa929d8bf3b007007639edea658c10497d
MacJ13 <jacdim02@gmail.com>
2023-10-27 22:47:26 +0200

    create constant 'HORIZONTAL', 'VERTITAL' variables

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -2,3 +2,6 @@ export const SHIP_SIZES = [4, 4, 3, 3, 2, 2, 1, 1];
 
 export const CUSTOM_ALPHABET = "1234567890abcdef";
 export const SIZE_ID = 10;
+
+export const HORIZONTAL = "horizontal";
+export const VERTICAL = "vertical";

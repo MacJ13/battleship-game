95e85a5091ccf1277b3359175f4bcb552bf54e39
MacJ13 <jacdim02@gmail.com>
2023-10-31 15:23:10 +0100

    Install HTMLWebpackplugin plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    assetModuleFilename: "assets/[name][ext]",
     clean: true,
   },
   devtool: "inline-source-map",
@@ -20,7 +22,7 @@ module.exports = {
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
-            options: { publicPath: "../" },
+            options: { publicPath: "./" },
           },
           ,
           "css-loader",
@@ -30,13 +32,26 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
+        generator: {
+          filename: "[name][ext]",
+        },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
+        generator: {
+          filename: "[name][ext]",
+        },
       },
     ],
   },
 
-  plugins: [new MiniCssExtractPlugin({ filename: "style.css" })],
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "style.css" }),
+    new HTMLWebpackPlugin({
+      title: "Battleship Game",
+      filename: "index.html",
+      template: path.resolve(__dirname, "./src/template.html"),
+    }),
+  ],
 };

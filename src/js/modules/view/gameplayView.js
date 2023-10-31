b050e3fd335fbb04ad81d5307747b3b65d98c1ef
MacJ13 <jacdim02@gmail.com>
2023-10-31 23:26:52 +0100

    Update gameplayView class
    create 'renderGameElement', 'renderCurrentResetBtn',
    'clearCurrentResetBtn' methods
    modify renderPlayerTurnPlease enter the commit message for your changes. Lines starting

--- src/js/modules/view/gameplayView.js
+++ src/js/modules/view/gameplayView.js
@@ -17,6 +17,7 @@ class GameplayView {
   playGameButton = document.getElementById("play");
   randomShipButton = document.getElementById("random");
   resetShipButton = document.getElementById("reset");
+  resetCurrentGame;
 
   draggedEl = null;
 
@@ -149,13 +150,43 @@ class GameplayView {
       partEl.classList.add("game-cell-ship");
     }
   }
+  renderGameElement(html) {
+    this.gameEl.insertAdjacentHTML("beforeend", html);
+  }
 
   renderPlayerTurn(name) {
     const html = `<div class="game-turn">
     <div class="game-current-name opaque">${name}'s turn</div>
   </div>`;
-    this.gameEl.insertAdjacentHTML("beforeend", html);
+    this.renderGameElement(html);
     // this.gameEl.insertAdjacentHTML("afterbegin", html);
+  }
+
+  renderCurrentResetBtn(cb) {
+    const html = `<div class="game-reset">
+    <button id="reset-current-game" class="btn-main" >
+    restart game
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 50 50"
+        width="100px"
+        height="100px"
+      >
+        <path
+          d="M 25 2 A 2.0002 2.0002 0 1 0 25 6 C 35.517124 6 44 14.482876 44 25 C 44 35.517124 35.517124 44 25 44 C 14.482876 44 6 35.517124 6 25 C 6 19.524201 8.3080175 14.608106 12 11.144531 L 12 15 A 2.0002 2.0002 0 1 0 16 15 L 16 4 L 5 4 A 2.0002 2.0002 0 1 0 5 8 L 9.5253906 8 C 4.9067015 12.20948 2 18.272325 2 25 C 2 37.678876 12.321124 48 25 48 C 37.678876 48 48 37.678876 48 25 C 48 12.321124 37.678876 2 25 2 z"
+        ></path>
+      </svg>
+    </button>
+  </div>`;
+    this.renderGameElement(html);
+    this.resetCurrentGame = document.getElementById("reset-current-game");
+    this.resetCurrentGame.addEventListener("click", cb);
+  }
+
+  clearCurrentResetBtn() {
+    const resetCurrentGameEl = this.gameEl.querySelector(".game-reset");
+    this.gameEl.removeChild(resetCurrentGameEl);
+    this.resetCurrentGame = null;
   }
 
   clearPlayerTurn() {
